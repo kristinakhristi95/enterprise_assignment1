4387924bcae4b185f0669e0d273e5dad258beee2
kristinakhristi95 <krishchristian.9@gmail.com>
2024-09-29 16:59:10 -0400

    request and response information

--- index.js
+++ index.js
@@ -3,21 +3,27 @@ const app = express();
 
 let products=[]
 
+// make 2 variable that will count the request for GET and POST
+
 app.use(express.json())
 
 app.get('/products', (req,res)=>{
-    
+    console.log(`Received Request`)
     res.json(products)
 })
 
 app.post('/products',(req,res)=>{
 
     const bodyData = req.body
-    // console.log(bodyData)
+    console.log(`Sending Response`)
     products.push(bodyData)
-    res.json({messsage:"Post request"})
+    res.json(bodyData)
 })
 
+app.delete('/products',(req,res)=>{
+    products = []
+    res.json({message:"Data deleted"})
+})
 
 app.listen(5000, ()=>{
     console.log(`Server is listening at 5000. http://localhost:5000/`)
