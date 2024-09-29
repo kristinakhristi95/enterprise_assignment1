b1fc6a488a1c56493a360d1b9659de9906518a54
kristinakhristi95 <krishchristian.9@gmail.com>
2024-09-29 17:42:30 -0400

    Start-up logging

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 const express = require('express');
 const app = express();
+const port = 5000;
 
 let products=[]
 
 // make 2 variable that will count the request for GET and POST
+let getRequestCount = 0;
+let postRequestCount = 0;
 
 app.use(express.json())
 
 app.get('/products', (req,res)=>{
+    getRequestCount++;
     console.log(`Received Request`)
     res.json(products)
 })
 
 app.post('/products',(req,res)=>{
 
+    postRequestCount++;
     const bodyData = req.body
+    console.log(bodyData)
     console.log(`Sending Response`)
     products.push(bodyData)
     res.json(bodyData)
@@ -22,9 +28,11 @@ app.post('/products',(req,res)=>{
 
 app.delete('/products',(req,res)=>{
     products = []
-    res.json({message:"Data deleted"})
+    res.json({message:"All products deleted"})
 })
 
-app.listen(5000, ()=>{
-    console.log(`Server is listening at 5000. http://localhost:5000/`)
+// Start-up logging
+app.listen(port, ()=>{
+    console.log(`Server is listening at http://127.0.0.1:${port}`);
+    console.log(`Endpoints:\nhttp://127.0.0.1:${port}/products method: GET, POST, DELETE`);
 })
